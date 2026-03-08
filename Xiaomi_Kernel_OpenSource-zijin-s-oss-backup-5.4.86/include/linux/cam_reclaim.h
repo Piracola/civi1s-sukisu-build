4d8e2959fea29f45f4e5f18e0e637b701afff584
civi1s-sukisu-build <civi1s-sukisu-build@local>
2026-03-08 15:34:01 +0800

    chore: 移除内核备份目录和KERNEL_VERSION文件
    删除 Xiaomi_Kernel_OpenSource-zijin-s-oss-backup-5.4.100 备份目录
    删除 KERNEL_VERSION 文件（已被版本管理系统替代）

--- Xiaomi_Kernel_OpenSource-zijin-s-oss-backup-5.4.86/include/linux/cam_reclaim.h
+++
@@ -1,2 +0,0 @@
-extern bool cam_reclaim(const char *name);
-extern int cam_reclaim_anonprivate(void);
--- Xiaomi_Kernel_OpenSource-zijin-s-oss-backup-5.4.86/techpack/bootinfo/bootinfoconf.h
+++
@@ -1,6 +0,0 @@
-/*
- * Copyright (C) 2011 Xiaomi Ltd.
- */
-#ifdef CONFIG_POWER_RESET_MSM
-#define CONFIG_BOOT_INFO 1
-#endif
